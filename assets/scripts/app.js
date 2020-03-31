ce3607a11ecfcb0039dae94aee901c7179f5d7ea
Artem Rybakov <gizmo3000@yandex.ru>
2020-03-31 14:35:14 +0300

    add different operations on calculator buttons

---
+++ assets/scripts/app.js
@@ -0,0 +1,64 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', devide);
+
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+    operatorIdentifier,
+    prevResult,
+    number,
+    result
+) {
+    const logEntry = {
+        operation: operatorIdentifier,
+        prevResult: prevResult,
+        number: number,
+        result: result
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function devide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+    writeToLog('DEVIDE', initialResult, enteredNumber, currentResult);
+}
